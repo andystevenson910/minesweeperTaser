1bfbedfd728c0356cb50eff63d6a99b918d7b230
andystevenson910 <andystevenson910@gmail.com>
2023-10-19 10:02:15 -0600

    tampermonkeyscript written

---
+++ tamperMonkeyScript.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Minesweeper Monitor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Monitor minesweeper.com for losses
+// @author       You
+// @match        https://minesweeperonline.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const elementSelector = '#face'; // Replace with your specific element's selector
+    const lostClassName = 'facedead'; // Replace with the class that indicates a loss
+
+    let targetElement = document.querySelector(elementSelector);
+
+    if (targetElement) {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                if (mutation.attributeName === 'class' && targetElement.classList.contains(lostClassName)) {
+                    console.log('Lost!');
+                    document.body.style.backgroundColor = "red";
+                } else {
+                    document.body.style.backgroundColor = "";
+                }
+            });
+        });
+
+        observer.observe(targetElement, {
+            attributes: true,
+            childList: false,
+            subtree: false
+        });
+    }
+
+})();
